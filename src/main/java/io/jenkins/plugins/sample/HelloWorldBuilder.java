65cc21470a5ca3384193490f193a1ed5548d66f3
Alex Wyllie <alex@dilex.net>
2018-04-21 15:50:47 -0400

    Now does what it is supposed to do.
    And by that, not say hello world. Furthermore, it reports on newly passing and failing tests, and also reminds you about tests that have been failing for a while.

--- src/main/java/io/jenkins/plugins/sample/HelloWorldBuilder.java
+++ src/main/java/io/jenkins/plugins/sample/HelloWorldBuilder.java
@@ -8,6 +8,7 @@ import hudson.model.AbstractProject;
 import hudson.model.Run;
 import hudson.model.TaskListener;
 import hudson.tasks.Builder;
+import hudson.tasks.junit.CaseResult;
 import hudson.tasks.junit.TestResult;
 import hudson.tasks.test.AggregatedTestResultAction;
 import hudson.tasks.BuildStepDescriptor;
@@ -31,28 +32,35 @@ public class HelloWorldBuilder extends Builder implements SimpleBuildStep {
 	private String output;
 
 	@DataBoundConstructor
-	public HelloWorldBuilder(String testSuiteFile) {
-		this.testSuiteFile = testSuiteFile;
+	public HelloWorldBuilder() {
+		this.testSuiteFile = "";
+		this.compileResults = true;
 	}
 
 	@DataBoundSetter
 	public void setCompileResults(boolean compileResults) {
 		this.compileResults = compileResults;
 	}
+	
 
 	@Override
 	public void perform(Run<?, ?> run, FilePath workspace, Launcher launcher, TaskListener listener) throws InterruptedException, IOException {
-		
+		if (!compileResults) {
+			return;
+		}
+		output = "";
 		listener.getLogger().println("Running my plugin...");
 
 		//Get current test results
 		//Also look up AbstractTestResultAction
 		AggregatedTestResultAction currentTestResults = run.getAction(AggregatedTestResultAction.class);
-
+		
 		//retrieve the test results 
 		List<AggregatedTestResultAction.ChildReport> currentResults = currentTestResults.getResult();
 
-
+		ArrayList<CaseResult> newlyPassed = new ArrayList<CaseResult>();
+		ArrayList<CaseResult> newlyFailed = new ArrayList<CaseResult>();
+		ArrayList<CaseResult> stillFailed = new ArrayList<CaseResult>();
 		//iterate through the result of each test
 		for(int i = 0; i < currentResults.size(); i++)
 		{
@@ -60,43 +68,78 @@ public class HelloWorldBuilder extends Builder implements SimpleBuildStep {
 			//obtain the report of a test	
 			AggregatedTestResultAction.ChildReport child = currentResults.get(i);
 
-			//retreive the test result
+			//retrieve the test result
 			TestResult currentTestResultChild = (TestResult)child.result;
 
 			//get the passed tests 
-			ArrayList<TestResult> currRes = ((ArrayList<TestResult>)currentTestResultChild.getPassedTests());
-
+			ArrayList<CaseResult> currRes = ((ArrayList<CaseResult>)currentTestResultChild.getPassedTests());
+			ArrayList<CaseResult> failedTests = ((ArrayList<CaseResult>)currentTestResultChild.getFailedTests());
+			
 			//iterate through each passed test
 			for(int j = 0; j < currRes.size(); j++)
 			{
 				//obtain the status of the test in current build
-				TestResult currentTestResChild = currRes.get(j);
-				
-				System.out.println(currentTestResChild);
+				CaseResult currentTestResChild = currRes.get(j);
 
 				//obtain the status of the test in previous build
-				TestResult previousTestResChild = (TestResult) currRes.get(j).getPreviousResult();
+				CaseResult previousTestResChild = (CaseResult) currRes.get(j).getPreviousResult();
 				
-				//Figure out how to compare them. Build output from this.
+				if (currentTestResChild.isPassed() && previousTestResChild.isFailed()) {
+					newlyPassed.add(currentTestResChild);
+				}
+			}
+			
+			//iterate through each failed test
+			for (int j = 0; j < failedTests.size(); j++) {
+				//obtain the status of the test in current build
+				CaseResult currentTestResChild = failedTests.get(j);
 				
+				//obtain the status of the test in the previous build
+				CaseResult previousTestResChild = (CaseResult) failedTests.get(j).getPreviousResult();
+				
+				if (currentTestResChild.isFailed() && previousTestResChild.isPassed()) {
+					newlyFailed.add(currentTestResChild);
+				}
+				else {
+					stillFailed.add(currentTestResChild);
+				}
 			}
 
 		}
-
+		
+		StringBuilder toOutput = new StringBuilder();
+		toOutput.append("-------------------------------------------------------\n N E W L Y    P A S S I N G   T E S T S\n-------------------------------------------------------\n");
+		for (int i = 0; i < newlyPassed.size(); i++) {
+			toOutput.append(newlyPassed.get(i).getName() + "\n");
+		}
+		toOutput.append("-------------------------------------------------------\n N E W L Y    F A I L I N G   T E S T S\n-------------------------------------------------------\n");
+		for (int i = 0; i < newlyFailed.size(); i++) {
+			toOutput.append(newlyFailed.get(i).getName() + "\n");
+		}
+		toOutput.append("-------------------------------------------------------\n S T I L L    F A I L I N G   T E S T S\n-------------------------------------------------------\n");
+		for (int i = 0; i < stillFailed.size(); i++) {
+			toOutput.append(stillFailed.get(i).getName() + "\n");
+		}
+		
+		output = toOutput.toString();
+		
+		listener.getLogger().println(output);
+		
 		//obtain the information from previous build
-		Run<?, ?> previousBuiltBuild = run.getPreviousBuiltBuild();
+		//Run<?, ?> previousBuiltBuild = run.getPreviousBuiltBuild();
 
 		
 		// try to read the test suite file from the workspace
-		InputStream inputStream = workspace.child(testSuiteFile).read();
+		//InputStream inputStream = workspace.child(testSuiteFile).read();
 
 		run.addAction(new OutputAction(output));
 	}
 
+	@Symbol("greet")
 	@Extension
 	public static final class DescriptorImpl extends BuildStepDescriptor<Builder> {
 
-		public FormValidation doCheckName(@QueryParameter String value, @QueryParameter boolean useFrench)
+		/*public FormValidation doCheckName(@QueryParameter String value, @QueryParameter boolean useFrench)
 				throws IOException, ServletException {
 			if (value.length() == 0)
 				return FormValidation.error(Messages.HelloWorldBuilder_DescriptorImpl_errors_missingName());
@@ -106,7 +149,7 @@ public class HelloWorldBuilder extends Builder implements SimpleBuildStep {
 				return FormValidation.warning(Messages.HelloWorldBuilder_DescriptorImpl_warnings_reallyFrench());
 			}
 			return FormValidation.ok();
-		}
+		}*/
 
 		@Override
 		public boolean isApplicable(Class<? extends AbstractProject> aClass) {
@@ -115,7 +158,7 @@ public class HelloWorldBuilder extends Builder implements SimpleBuildStep {
 
 		@Override
 		public String getDisplayName() {
-			return Messages.HelloWorldBuilder_DescriptorImpl_DisplayName();
+			return "Compare test results to previous builds";
 		}
 
 	}
